541f498fc2901f91faa38a84c87460af144a11ae
CaptainBoo <eddiethebest06@gmail.com>
2025-01-15 16:52:02 -0500

    pathfinding

--- ISU-Escapists/src/Person.java
+++ ISU-Escapists/src/Person.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 abstract public class Person {
 
@@ -59,14 +60,19 @@ abstract public class Person {
 		return frameIndex;
 	}
 
-	public void findRandomPoint(int mapLength, int mapWidth) {
-//		destX = (int) (Math.random() * mapLength + 1);
-//		destY = (int) (Math.random() * mapWidth + 1);
-//		System.out.println("Point is " + destX + " " + destY);
-//		currentlyPathfinding = true;
+	public void movement(int[][]grid, int mapLength, int mapWidth) {
+		destX = (int) (Math.random() * mapLength + 1);
+		destY = (int) (Math.random() * mapWidth + 1);
+		System.out.println("Point is " + destX + " " + destY);
+		currentlyPathfinding = true;
+		
+		List<int[]> path = findPath(grid, new int[]{x/29,y/29}, new int[]{destX/29,destY/29});
+		System.out.println(path);
 	}
 
-	public ArrayList<int[]> findPath(int[][] grid, int[] start, int[] goal) {
+	public List<int[]> findPath(int[][] grid, int[] start, int[] goal) {
+		int[]dx = {0, 1, 0, -1}; // Directions: Right, Down, Left Up
+		int[]dy = {1, 0, -1, 0};
 		int rows = grid.length;
 		int cols = grid.length;
 		
@@ -75,7 +81,33 @@ abstract public class Person {
 		
 		while(!queue.isEmpty()) {
 			int[] currentPos = queue.poll();
+			int cx = currentPos[0], cy = currentPos[1];
+			
+			if (cx == goal[0] && cy == goal[1]) {
+				List<int[]> path = new ArrayList<>();
+				while(grid[cx][cy] != -1) {
+					path.add(new int[] {cx,cy});
+					int temp = grid[cx][cy];
+					cx = temp / cols;
+					cy = temp % cols;
+				}
+				path.add(start);
+				Collections.reverse(path);
+				return path;
+			}
+			
+			for (int i = 0; i < 4; i++) {
+				int nx = cx + dx[i];
+				int ny = cy + dy[i];
+				if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && grid[nx][ny] == 0) {
+					grid[nx][ny] = cx * cols + cy; // Store the parent info directly in the grid
+					queue.add(new int[] {nx, ny});
+				}
+			}
 		}
+		return Collections.emptyList();
+		
+	
 		
 //		if (!currentlyPathfinding) {
 //			findRandomPoint(1000, 1000);
